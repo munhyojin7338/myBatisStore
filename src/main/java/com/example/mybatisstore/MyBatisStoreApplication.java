0a96c65588bc2928ccca92d1753e2cb28da9346d
MUN-HyoJin <hyojin7338@naver.com>
2023-11-29 16:19:45 +0900

    myBatis을 사용하여 Store 사용해보기

---
+++ src/main/java/com/example/mybatisstore/MyBatisStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybatisstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mybatisstore/MyBatisStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatisstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
