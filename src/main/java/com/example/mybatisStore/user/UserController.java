ded87c9ed04f0f2d6439f42723fb7c1cdade1afa
MUN-HyoJin <hyojin7338@naver.com>
2023-12-09 15:55:19 +0900

    User 초기 설정 완료,  UserMapper 수정

--- src/main/java/com/example/mybatisStore/user/UserController.java
+++ src/main/java/com/example/mybatisStore/user/UserController.java
@@ -1,7 +1,6 @@
 package com.example.mybatisStore.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/com/example/mybatisStore/user/UserMapper.java
+++ src/main/java/com/example/mybatisStore/user/UserMapper.java
@@ -4,16 +4,9 @@ import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface UserMapper {
-    @Select("SELECT * FROM user WHERE id = #{id}")
-    @Results({
-            @Result(column = "id", property = "id"),
-            @Result(column = "username",property = "username"),
-            @Result(column="email" ,property="userEmail"),
-            @Result(column="password" ,property="password" ),
-            @Result(column="phone" ,property="phone"),
-            @Result(column="age" ,property="age" ),
-            @Result(column="address" ,property="address")
-    })
+    @Select("SELECT * id, username, email, password, phone, age, address " +
+            "FROM user WHERE id = #{id}")
+    @ResultMap("userResultMap")
     User findById(Long id);
 
     @Insert("INSERT INTO user (gender) VALUES (#{gender})") // 성별
