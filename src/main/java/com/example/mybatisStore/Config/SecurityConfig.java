8407e8401cf22606bbd827e274674e670a3a4ec0
MUN-HyoJin <hyojin7338@naver.com>
2023-12-23 23:57:02 +0900

    스프링 시큐리티 수정본

--- src/main/java/com/example/mybatisStore/Config/SecurityConfig.java
+++ src/main/java/com/example/mybatisStore/Config/SecurityConfig.java
@@ -1,23 +1,41 @@
 package com.example.mybatisStore.Config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  {
 
+/*
+업데이트 사항대로
+
+authorizeRequest() => authorizeHttpRequests()로 수정
+antMatchers() => requestMatchers()로 수정
+ */
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable
+                )
+                .headers((headerConfig) ->
+                        headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                )
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .requestMatchers("/", "/**").permitAll()
+                                .requestMatchers("/testPage","/testPage").hasRole(Role.ADMIN.name())
+                                .anyRequest().authenticated()
+                );
 
-        http.csrf((csrf) -> csrf.disable())
-                .authorizeHttpRequests((requests)->requests
-                        .anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
