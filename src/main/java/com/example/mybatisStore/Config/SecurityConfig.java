338fcf4e6029431551ea4db8316477874bc62556
MUN-HyoJin <hyojin7338@naver.com>
2023-12-25 03:17:24 +0900

    스프링 시큐리티 수정본

--- src/main/java/com/example/mybatisStore/Config/SecurityConfig.java
+++ src/main/java/com/example/mybatisStore/Config/SecurityConfig.java
@@ -1,20 +1,28 @@
 package com.example.mybatisStore.Config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  {
 
 /*
-업데이트 사항대로
+업데이트
+
+// before
+.csrf().disable()
+// after
+.csrf(AbstractHttpConfigurer::disable) -> : Cross-Site Request Forgery (CSRF)
+공격을 방지하기 위한 CSRF 보호를 비활성화합니다.
+
 
 authorizeRequest() => authorizeHttpRequests()로 수정
 antMatchers() => requestMatchers()로 수정
@@ -22,20 +30,15 @@ antMatchers() => requestMatchers()로 수정
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable
-                )
-                .headers((headerConfig) ->
-                        headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
-                )
-                .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests
-                                .requestMatchers(PathRequest.toH2Console()).permitAll()
-                                .requestMatchers("/", "/**").permitAll()
-                                .requestMatchers("/testPage","/testPage").hasRole(Role.ADMIN.name())
-                                .anyRequest().authenticated()
-                );
 
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(( authorizeRequests)-> authorizeRequests
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().permitAll())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(withDefaults()); // HttpSecurity
         return http.build();
     }
 
