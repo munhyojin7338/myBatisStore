07d73f719ea1ec25555eb424b302644ab976a837
MUN-HyoJin <hyojin7338@naver.com>
2024-01-11 01:48:23 +0900

    Board 와 Store 객체 변경

--- src/main/java/com/example/mybatisStore/store/service/StoreService.java
+++ src/main/java/com/example/mybatisStore/store/service/StoreService.java
@@ -1,10 +1,11 @@
 package com.example.mybatisStore.store.service;
 
+import com.example.mybatisStore.store.Store;
 import com.example.mybatisStore.store.dto.StoreRegisterDto;
-import com.example.mybatisStore.user.User;
-import org.springframework.security.core.Authentication;
 
 public interface StoreService {
 
-    Long getCreate(StoreRegisterDto registerDto);
+    Long getCreate(Long userId, StoreRegisterDto registerDto);
+
+    Store getProductById(Long productId);
 }
