71baa50f60c6ca5d0e8349783ea646d96ea68eb0
MUN-HyoJin <hyojin7338@naver.com>
2024-01-14 21:37:42 +0900

    mybatis 수정 StoreMapper 수정

--- src/main/java/com/example/mybatisStore/store/service/StoreService.java
+++ src/main/java/com/example/mybatisStore/store/service/StoreService.java
@@ -1,10 +1,24 @@
 package com.example.mybatisStore.store.service;
 
+import com.example.mybatisStore.store.entity.Store;
+import com.example.mybatisStore.store.entity.dto.StoreRegisterDto;
+import com.example.mybatisStore.store.repository.StoreMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-
 public class StoreService {
 
+    private final StoreMapper storeMapper;
+
+    @Autowired
+    public StoreService(StoreMapper storeMapper) {
+        this.storeMapper = storeMapper;
+    }
+
+    public void addStore(StoreRegisterDto storeRegisterDto) {
+        Store store = storeRegisterDto.toEntity();
+        storeMapper.createStore(store);
+    }
 
 }
